1174bf0aaa38c6896d3b429366ef27c821544c6e
Jorden <knock007@alumni.umn.edu>
2025-08-05 10:55:23 -0500

    error fixes

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -8,19 +8,13 @@ import z from "zod";
 
 // Events Page Props declaration
 type EventsPageProps = {
-  params: {
-    city: string;
-  };
-};
-
-// Events Page Props + Search Params declaration
-type NewEventsPageProps = EventsPageProps & {
-  searchParams: { [key: string]: string | string[] | undefined };
-};
+  params: Promise<{ city: string }>
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+}
 
 // Generate Metadata based on city
-export function generateMetadata({ params }: EventsPageProps): Metadata {
-  const city = params.city;
+export async function generateMetadata({ params }: EventsPageProps): Promise<Metadata> {
+  const { city } = await params;
   return {
     title: city === "all" ? "All Events" : `Events in ${capitalize(city)}`,
   };
@@ -29,11 +23,12 @@ export function generateMetadata({ params }: EventsPageProps): Metadata {
 export default async function Events({
   params,
   searchParams,
-}: NewEventsPageProps) {
-  const city = params.city;
+}: EventsPageProps) {
+  const { city } = await params;
+  const { page } = await searchParams
   // Use zod to check for valid page number
   const pageNumberSchema = z.coerce.number().int().positive().optional();
-  const parsedPage = pageNumberSchema.safeParse(searchParams.page);
+  const parsedPage = pageNumberSchema.safeParse(page);
   if (!parsedPage.success) {
     throw new Error("Invalid page number");
   }
